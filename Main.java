f43b85a366a9210c0d7b3ac9115a6cd7feda52a1
ShishkinaElena <148252907+ShishkinaElena@users.noreply.github.com>
2023-10-25 13:48:37 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,81 @@
+package Homework5kalkyl;
+/*ДЗ доработать калькулятор на использование методов,
+разнести функционал по методам: сумма отдельно, разность отдельно и т.д
+Выполнить запрос новых данных в бесконечный цикл*/
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (; ; ) {
+            a();
+            int a = scanner.nextInt();
+            b();
+            int b = scanner.nextInt();
+            d();
+            int d = scanner.nextInt();
+
+            if (d == 1) {
+                sums(a, b);
+            } else if (d == 2) {
+                differences(a, b);
+            } else if (d == 3) {
+                multipliers(a, b);
+            } else if (d == 4) {
+                divisions(a, b);
+
+            } else if (d>4 || d<0){
+                System.out.println("ERROR");
+            }
+            continue;
+        }
+    }
+    static void a() {
+        System.out.print("Введите первое число: ");
+    }
+    static void b() {
+        System.out.print("Введите второе число: ");
+    }
+    static void d() {
+        System.out.print("Введите число-действие от 1 до 4 соответственно '+' '-' '*' '/': ");
+    }
+    public static void sums(int a, int b) {
+
+        int j = a + b;
+        System.out.println("Сумма=  " + j);
+    }
+    public static void differences(int a, int b) {
+        int j = a - b;
+        System.out.println("Разность = " + j);
+    }
+    public static void multipliers(int a, int b) {
+        int j = a * b;
+        System.out.println("Произведение = " + j);
+    }
+    public static void divisions(int a, int b) {
+        double j = (double) a / (double) b ;
+                   System.out.println("Частное = " + j);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
